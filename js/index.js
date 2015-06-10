f5dfe29068bd52f5b658d18c6d9a32d7883dda2a
Hogue <ajhogue6@gmail.com>
2015-06-09 21:31:37 -0400

    adds ajax click handler and append method

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
   $(document).ready(function(){
+    $('#magic-button').on('click', function() {
+
+    })
     var root = 'http://jsonplaceholder.typicode.com';
     $.ajax({
       url: root + '/posts',
       method: 'GET'
     }).done(function(data) {
       console.log(data);
+      $("#list").html('');
+      data.forEach(function(object){
+        $("#list").append('<li><a href="">' + object.title + '</a></li>');
+      });
     });
   });
